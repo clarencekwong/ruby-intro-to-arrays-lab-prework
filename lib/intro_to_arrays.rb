a2b4c7f64e2dffcb3545446b0fd0e75764eb0dbd
Clarence Kwong <clarencekwong7@gmail.com>
2018-11-17 21:14:48 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -19,6 +19,12 @@ def last_element(array)
 end
 
 def first_element_with_array(array)
-  array.first()
+  array.first
 end
 
+def last_element_with_array(array)
+  array.last
+end
+
+def length_of_array(array)
+  array
